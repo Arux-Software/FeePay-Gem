b6428470a74c65ec5439f3802b5493da4fcf4971
Stephen Heuer <sheuer@int42.org>
2015-02-13 15:55:39 -0600

    fix attribute name

--- lib/fee_pay/api/auth.rb
+++ lib/fee_pay/api/auth.rb
@@ -9,7 +9,7 @@ module FeePay
         end
       end
       
-      attr_accessor :client_id, :client_secret, :redirect_uri, :js_callback, :district_subdomain, :current_user_id
+      attr_accessor :client_id, :client_secret, :redirect_uri, :js_callback, :district_subdomain, :current_user_uuid
       
       def initialize(options = {})
         self.client_id = options[:client_id]
