97dd5b57a2afacacccd1072408d6f32f6ef4307f
Stephen Heuer <sheuer@int42.org>
2015-02-13 15:52:52 -0600

    ensure nav is required

--- lib/fee_pay.rb
+++ lib/fee_pay.rb
@@ -7,10 +7,11 @@ require "fee_pay/api"
 require "fee_pay/api/bank_info"
 require "fee_pay/api/config"
 require "fee_pay/api/auth"
+require "fee_pay/api/nav"
 require "fee_pay/api/account"
 require "fee_pay/api/cart"
 
-module FeePay
-  VERSION = "0.0.2"
+m0.0.3 FeePay
+  VERSION = "0.0.3"
   USER_AGENT = "FeePay GEM #{VERSION}"
 end
