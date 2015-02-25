58cc5f1ac75138abb757407fd655ee5bd6be1777
Stephen Heuer <sheuer@int42.org>
2015-02-25 16:40:44 -0600

    add api for district student id and state student id lookups

--- lib/feepay.rb
+++ lib/feepay.rb
@@ -9,6 +9,7 @@ require "feepay/api/config"
 require "feepay/api/auth"
 require "feepay/api/nav"
 require "feepay/api/account"
+require "feepay/api/student"
 require "feepay/api/cart"
 
 module FeePay
